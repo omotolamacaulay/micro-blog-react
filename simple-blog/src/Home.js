6a55e288949c1ee29254bf772095866592ae8827
Omotola Macaulay <tolamacaulay@gmail.com>
2021-08-03 12:42:38 +0100

    connect to json server

--- simple-blog/src/Home.js
+++ simple-blog/src/Home.js
@@ -1,17 +1,19 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25)
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
+ const handleDelete = (id) => {
+   const newBlog = blogs.filter(blog => blog.id !== id);
+   setBlogs(newBlog);
+ }
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      <BlogList blogs={blogs} title= "All Blogs" handleDelete={handleDelete} />
     </div>
   );
 }
