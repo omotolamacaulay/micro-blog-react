24631eebca12681f6c75f957f4cfef61e0bf0a25
Omotola Macaulay <tolamacaulay@gmail.com>
2021-08-04 12:50:19 +0100

    load blogs with id

--- simple-blog/src/Home.js
+++ simple-blog/src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   
   return (
     <div className="home">
-      { error && <div>{ error }</div>}
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title= "All Blogs" />}
     </div>
